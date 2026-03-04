03e699fa0459d545f7f9a2eb0682a9d64e108a71
Unai2310 <78329457+Unai2310@users.noreply.github.com>
2026-03-04 14:48:00 +0100

    Express version downgrade

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const io = new Server(httpServer, {
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
